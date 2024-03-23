b708faa59a05c4539a868f22b22de02d98a3a0f2
Alimi Kehinde Morufudeen <alimi.kmaruf@gmail.com>
2024-03-23 12:49:07 +0100

    create dms react application

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Register from "./pages/register/Register";
+
+const App = () => {
+  return (
+    <div>
+      <Register />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/pages/register/Register.jsx
@@ -0,0 +1,61 @@
+import styles from "./register.module.css";
+import Logo from "../../images/Logo.svg";
+import BikeMan from "../../images/bike-man.svg";
+
+const Register = () => {
+  return (
+    <div className={styles.registerContainer}>
+      <header className={styles.logoContainer}>
+        <img src={Logo} alt="dms logo" className={styles.logo} />
+        <h3 className={styles.logoText}>dms</h3>
+      </header>
+
+      <section className={styles.registerContent}>
+        <section className={styles.leftContainter}>
+          <img src={BikeMan} alt="Bike Man" />
+          <h3 className={styles.title}>
+            Let’s help you manage your riders and delivery systems.
+          </h3>
+          <p className={styles.desc}>
+            Every logistics company whether it’s a multi-national branding
+            corporation or a regular local deserves the basic standard
+            necessities in achieving a high service delivery
+          </p>
+          <button className={styles.btn}>Get Started</button>
+        </section>
+        <section className={styles.rightContainter}>
+          <progress
+            className={styles.progressBar}
+            max={100}
+            value={70}
+          ></progress>
+          <section className={styles.formInfo}>
+            <h2>Basic Information</h2>
+            <p>Enter the following information below</p>
+          </section>
+          <section className={styles.formContainer}>
+            <form className={styles.form}>
+              <section className={styles.nameInput}>
+                <input type="text" placeholder="Company Name" />
+                <input type="text" placeholder="Last Name" />
+              </section>
+              <input type="text" placeholder="Company Address" />
+              <input type="tel" placeholder="Phone Number" />
+              <input type="email" placeholder="Company Email Address" />
+              <input type="password" placeholder="Password" />
+            </form>
+            <p className={styles.termAndCondition}>
+              By clicking "Next" you agree to our Terms of Service, Privacy
+              Policy, and to receive marketing communications from Envoy.
+            </p>
+            <section className={styles.account}>
+              <p>Got an account? Sign in</p>
+              <button className={styles.btn}>Register</button>
+            </section>
+          </section>
+        </section>
+      </section>
+    </div>
+  );
+};
+export default Register;
